0cf09235aa085134988bd94ee62644de7cabd712
qi891011 <1193553883@qq.com>
2018-09-25 15:50:06 +0800

    Update database.php

--- config/database.php
+++ config/database.php
@@ -13,13 +13,13 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => '115.28.245.108',
+    'hostname'        => '',
     // 数据库名
     'database'        => 'black',
     // 用户名
-    'username'        => 'root',
+    'username'        => '',
     // 密码
-    'password'        => 'WANGfu2016',
+    'password'        => '',
     // 端口
     'hostport'        => '3306',
     // 连接dsn
